75c87cdb8441b76262837b4ea752c4b539659e8d
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-06-02 15:55:06 -0300

    alteracao de rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -3,39 +3,48 @@ const nextConfig = {
   experimental: {
     middlewareClientMaxBodySize: 50 * 1024 * 1024,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/users/:path*",
-        destination: "http://localhost:8080/users/:path*",
-      },
-
-      {
-        source: "/refresh/:path*",
-        destination: "http://localhost:8080/refresh/:path*",
-      },
-
-      {
-        source: "/conversas/:path*",
-        destination: "http://localhost:8080/conversas/:path*",
-      },
-
-      {
-        source: "/private-messages/:path*",
-        destination: "http://localhost:8080/private-messages/:path*",
-      },
-
-      {
-        source: "/files/:path*",
-        destination: "http://localhost:8080/files/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/users/:path*",
+  //       destination: "http://localhost:8080/users/:path*",
+  //     },
+  //
+  //     {
+  //       source: "/refresh/:path*",
+  //       destination: "http://localhost:8080/refresh/:path*",
+  //     },
+  //
+  //     {
+  //       source: "/conversas/:path*",
+  //       destination: "http://localhost:8080/conversas/:path*",
+  //     },
+  //
+  //     {
+  //       source: "/private-messages/:path*",
+  //       destination: "http://localhost:8080/private-messages/:path*",
+  //     },
+  //
+  //     {
+  //       source: "/files/:path*",
+  //       destination: "http://localhost:8080/files/:path*",
+  //     },
+  //   ];
+  // },
   /* config options here */
   images: {
     remotePatterns: [{
       protocol: "http",
       hostname: "localhost",
+      port: "8080",
+      pathname: "**",
+      search: ""
+    }
+    ],
+
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "https://livechat-0380.onrender.com",
       port: "8080",
       pathname: "**",
       search: ""
