70eeda201a3712e4733a9d2b9fa1897fe29574ea
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-06-09 15:51:27 -0300

    jaiaksnsqkia

--- app/componentes/functions/getClient.js
+++ app/componentes/functions/getClient.js
@@ -5,12 +5,11 @@ let client = null;
 export function getClient() {
   if (client) return client
 
-  const socket = new SockJS("https://livechat-0380.onrender.com/buildrun-livechat-websocket");
+  const socket = new SockJS("https://livechat-0380.onrender.com/buildrun-livechat-websocket?user=" + localStorage.getItem("email"));
 
   //  const socket = new SockJS("https://speakflow.ddns.net/buildrun-livechat-websocket")
   client = new Client({
     webSocketFactory: () => socket, reconnectDelay: 5000,
-    connectHeaders: { user: localStorage.getItem("email") },
     debug: (str) => console.log(str)
   })
   client.activate()
