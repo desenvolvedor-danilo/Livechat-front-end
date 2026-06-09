8218b6e0f2537c574e32e9c39af3ae346d4c437d
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-06-09 19:01:26 -0300

    jzjzsuiqsk

--- app/componentes/utils/ExtractExtension.js
+++ app/componentes/utils/ExtractExtension.js
@@ -1,7 +1,7 @@
 export function ExtractExtension(element) {
   const indexOfImage = element.substring(element.lastIndexOf("/")
     + 1, element.length);
-  console.log(indexOfImage)
+  console.log("Este é o index: ", indexOfImage)
 
   const extension = indexOfImage.substring(indexOfImage.indexOf(".") + 1, indexOfImage.length)
   return extension
