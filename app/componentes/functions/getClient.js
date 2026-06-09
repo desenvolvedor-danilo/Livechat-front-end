86b60ee57ac64b68b28d7dde1749b09ef1fa08ab
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-06-09 15:45:52 -0300

    nznzjaiajznnaa

--- app/componentes/functions/getClient.js
+++ app/componentes/functions/getClient.js
@@ -5,7 +5,7 @@ let client = null;
 export function getClient() {
   if (client) return client
 
-  const socket = new SockJS("https://speakflow.ddns.net/buildrun-livechat-websocket");
+  const socket = new SockJS("https://livechat-0380.onrender.com/buildrun-livechat-websocket");
 
   //  const socket = new SockJS("https://speakflow.ddns.net/buildrun-livechat-websocket")
   client = new Client({
