f8688714bec09b2e0465fb00a3ba60e781387cda
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-07-13 13:12:28 -0300

    troca de icone de notificacoes via firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -22,7 +22,7 @@ messaging.onBackgroundMessage((payload) => {
 	console.log("[SW] LINK EXTRAÍDO:", link);
 	self.registration.showNotification(payload.notification.title, {
 		body: payload.notification.body,
-		icon: "/chat-icon.png",
+		icon: "/speakflow.png",
 		data: {
 			link: link,
 		},
