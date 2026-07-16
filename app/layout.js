ce1e5b5ac4c0448ffba7bdd9a5377f255fbf6cf5
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-07-16 09:02:20 -0300

    melhorias para o google analytics

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import "./css/notification.css"
 import "./css/globals.css";
 import "./css/list-contacts.css"
 import FirebaseServiceWorker from "./componentes/ui/workerRegistration";
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,8 +17,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Speakflow chat",
+  title: "Speakflow Chat",
   description: "Aqui a conversa flui",
+  applicationName: "Speakflow Chat"
 };
 
 export default function RootLayout({ children }) {
@@ -28,7 +30,27 @@ export default function RootLayout({ children }) {
       <body>
         <FirebaseServiceWorker />
         {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JCRR8G5JS1"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag(){
+            dataLayer.push(arguments);
+          }
+
+          gtag('js', new Date());
+
+          gtag('config', 'G-JCRR8G5JS1');
+        `}
+        </Script>
       </body>
+
     </html>
   );
 }
