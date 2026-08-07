e1e053f02adcd78c268be291462e9f4b4f2aea6d
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-08-07 09:17:48 -0300

    wllwkzkzmsmmwsmmzmwmwmmszzmsmwmsm

--- app/componentes/functions/getClient.js
+++ app/componentes/functions/getClient.js
@@ -3,13 +3,13 @@ import SockJS from "sockjs-client";
 
 let client = null;
 export function getClient() {
-  // const PRODUCTION_WS_URL = "https://speakflow.ddns.net/buildrun-livechat-websocket?user="
-  const LOCAL_WS_URL = "http://localhost:8080/buildrun-livechat-websocket?user="
+  const PRODUCTION_WS_URL = "https://speakflow.ddns.net/buildrun-livechat-websocket?user="
+  // const LOCAL_WS_URL = "http://localhost:8080/buildrun-livechat-websocket?user="
   if (client) return client
 
-  //  const socket = new SockJS(PRODUCTION_WS_URL + localStorage.getItem("email"));
+  const socket = new SockJS(PRODUCTION_WS_URL + localStorage.getItem("email"));
 
-  const socket = new SockJS(LOCAL_WS_URL + localStorage.getItem("email"));
+  // const socket = new SockJS(LOCAL_WS_URL + localStorage.getItem("email"));
 
   client = new Client({
     webSocketFactory: () => socket, reconnectDelay: 5000,
